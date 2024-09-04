1c536d745a1dd492d1f496d8fc723a6e0d74898b
paul-b-b64 <belousko@inbox.ru>
2024-09-04 17:19:05 +0300

    ДЗ 10.3

---
+++ Modul_10_3.py
@@ -0,0 +1,42 @@
+import threading
+from threading import Thread, Lock
+from random import randint
+from time import sleep
+
+
+class Bank:
+
+    def __init__(self):
+        self.balance = 0
+        self.lock = Lock()
+
+    def deposit(self):
+        for i in range(100):
+            summa = randint(50, 500)
+            self.balance += summa
+            print(f'Пополнение: {summa}. Баланс: {self.balance}')
+            if self.balance >= 500 and self.lock.locked():
+                self.lock.release()
+            sleep(0.001)
+
+    def take(self):
+        for i in range(100):
+            summa = randint(50, 500)
+            print(f'Запрос на: {summa}')
+            if summa <= self.balance:
+                self.balance -= summa
+                print(f'Снятие: {summa}. Баланс: {self.balance}')
+            else:
+                print('Запрос отклонен, недостаточно средств')
+                self.lock.acquire()
+
+bk = Bank()
+th1 = threading.Thread(target=Bank.deposit, args=(bk,))
+th2 = threading.Thread(target=Bank.take, args=(bk,))
+
+th1.start()
+th2.start()
+th1.join()
+th2.join()
+
+print(f'Итоговый баланс: {bk.balance}')
