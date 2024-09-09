01d564a34145f39d7a09b997c2a3914e4d38ab9e
paul-b-b64 <belousko@inbox.ru>
2024-09-09 11:36:06 +0300

    ДЗ 10.4

--- Modul_10_4.py
+++ Modul_10_4.py
@@ -38,24 +38,24 @@ class Cafe:
                     if table.guest == None:
                         table.guest = guest
                         guest.start()
-                        self.guests.append(guest)
+                        guest.join()
                         print(f'{guest.name} сел(-а) за стол номер {table.number}')
                         break
 
     def discuss_guests(self):
-        # try:
-        for table in self.tables:
-            if table.guest != None and not table.guest.is_alive():
-                print(f'{table.guest.name} покушал и ушел')
-                print(f'Стол номер {table.number} свободен')
-                table.guest = None
-            if table.guest == None and not self.queue.empty():
-                table.guest = self.queue.get()
-                print(f'{table.guest.name} вышел из очереди и сел за стол номер {table.number}')
-                table.guest.start()
-        #
-        # except self.queue.empty():
-        #     print('Очередь пуста')
+        while not self.queue.empty() and not all(table.guest == None for table in self.tables):
+            for table in self.tables:
+                if table.guest == None and not self.queue.empty():
+                    guest = self.queue.get()
+                    table.guest = guest
+                    guest.start()
+                    guest.join()
+                    print(f'{table.guest.name} вышел из очереди и сел за стол номер {table.number}')
+                elif table.guest != None and not table.guest.is_alive():
+                    print(f'{table.guest.name} покушал и ушел')
+                    print(f'Стол номер {table.number} свободен')
+                    table.guest = None
+                    break
 
 
 tables = [Table(number) for number in range(1, 6)]
@@ -71,6 +71,3 @@ cafe = Cafe(*tables)
 cafe.guest_arrival(*guests)
 # Обслуживание гостей
 cafe.discuss_guests()
-# for guest in guests:
-#     guest.join()
-#
